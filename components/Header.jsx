f04596a44ab06152c799bc231cc7721c4b47b69e
Saugat Rimal <saugatrimal60@gmail.com>
2022-11-23 23:03:46 +0545

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,7 +16,7 @@ export const Header = () => {
         <div className="md:float-left" block>
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              GraphCMS
+              Syntax Expert
             </span>
           </Link>
         </div>
