cc52edd5dacfccaf47660f6853f6baff77d57495
Saugat Rimal <saugatrimal60@gmail.com>
2022-11-23 22:27:26 +0545

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
-import { PostCard, Categories, PostWidget } from '../components';
-import { getPosts } from '../services';
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import {PostCard, Categories, PostWidget} from '../components'
+import {getPosts} from '../services'
 
-export default function Home({ posts }) {
+
+
+const Home: NextPage = ({posts}) => {
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container mx-auto px-10 mb-8 ">
+
+      <Head>
+        <title>CMS Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
@@ -16,15 +26,21 @@ export default function Home({ posts }) {
             <Categories />
           </div>
         </div>
+
+    
       </div>
+
+
     </div>
-  );
+  )
 }
 
-// Fetch data at build time
-export async function getStaticProps() {
+export default Home
+
+export async function getStaticProps(){
   const posts = (await getPosts()) || [];
-  return {
-    props: { posts },
-  };
+
+  return{
+    props : { posts}
+  }
 }
